6c9b096b711ac8219e63cc8ad0ae25c1e9f47945
caojunjie <1301239018@qq.com>
2024-07-27 15:09:23 +0800

    chore(build)

--- main/bridge/env.js
+++ main/bridge/env.js
@@ -1,4 +1,4 @@
-const packageJson = require('../package.json');
+const packageJson = require('../../package.json');
 
 process.versions['app'] = packageJson.version;
 process.versions['name'] = packageJson.name;
